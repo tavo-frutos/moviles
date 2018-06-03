211dd41cee78469bb2e7ceb705925182fe935f40
Gustavo Adolfo Frutos Barriga <tavo@MacBook-Pro-de-Gustavo.local>
2018-06-02 21:18:31 -0500

    Version 1 login facebook

---
+++ Anime_final/app/src/main/java/com/example/gustavo/anime_final/Inicio.kt
@@ -0,0 +1,12 @@
+package com.example.gustavo.anime_final
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
---
+++ Anime_final/app/src/main/java/com/example/gustavo/anime_final/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.gustavo.anime_final
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var callbackManager: CallbackManager? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var btnLoginFacebook = findViewById<Button>(R.id.login_button)
+
+        btnLoginFacebook.setOnClickListener(View.OnClickListener {
+            // Login
+            callbackManager = CallbackManager.Factory.create()
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+            LoginManager.getInstance().registerCallback(callbackManager,
+                    object : FacebookCallback<LoginResult> {
+                        override fun onSuccess(loginResult: LoginResult) {
+                            Log.d("MainActivity", "Facebook token: " + loginResult.accessToken.token)
+                            Toast.makeText(this@MainActivity, "Sí jala!", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(applicationContext, Inicio::class.java))
+                        }
+
+                        override fun onCancel() {
+                            Log.d("MainActivity", "Facebook onCancel.")
+
+                        }
+
+                        override fun onError(error: FacebookException) {
+                            Log.d("MainActivity", "Facebook onError.")
+                            Toast.makeText(this@MainActivity, "Mierda!", Toast.LENGTH_SHORT).show()
+
+                        }
+                    })
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+}
